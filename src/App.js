bf11cec526ffe452be783a51d00b554c7ac5f9eb
Gabi <sz.gabor199654@gmail.com>
2020-12-02 22:43:52 +0100

    5.Intro to styled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 // Import pages
+// Global Style
+import GlobalStyle from "./components/GlobalStyle";
 import AboutUs from "./pages/AboutUs";
 
 const App = () => {
   return (
     <div className="App">
       <h1>Film</h1>
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
